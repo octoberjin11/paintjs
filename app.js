24d845b76a42b50bc2c9f3ca3db85318b10142e9
octoberjin11 <octoberjin11@gmail.com>
2021-09-28 11:02:56 +0900

    Canvas Events

--- app.js
+++ app.js
@@ -7,25 +7,22 @@ function stopPainting() {
 }
 
 function onMouseMove(event) {
-  //마우스 움직임 감지
   const x = event.offsetX;
   const y = event.offsetY;
   console.log(x, y);
 }
 
 function onMouseDown(event) {
-  //마우스 클릭 감지 (마우스를 클릭하고 있을때)
   painting = true;
 }
 
 function onMouseUp(event) {
-  //마우스 클릭 감지 (마우스를 클릭을 그만뒀을때)
   stopPainting();
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousemove", onMouseMove); //마우스 움직임 감지
+  canvas.addEventListener("mousedown", onMouseDown); //마우스 클릭 감지 (마우스를 클릭하고 있을때)
+  canvas.addEventListener("mouseup", onMouseUp); //마우스 클릭 감지 (마우스를 클릭을 그만뒀을때)
   canvas.addEventListener("mouseleave", stopPainting); //마우스가 컨버스 영역을 벗어났을때 감지
 }
